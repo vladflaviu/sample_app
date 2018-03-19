a5877207c5ae0398cec54eb90f8f64ea2e93f2e4
Vlad  Irimies <vladirimies@Vlads-MacBook-Pro.local>
2018-03-19 12:52:59 +0200

    before merge

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
 
   test "should get home" do
